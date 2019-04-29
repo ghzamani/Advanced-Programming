fbd48725cd542299f48bb0b82dd622897d8116cf
ghzamani <ghzamaninejad@gmail.com>
2019-04-29 13:41:05 +0430

    Exam

---
+++ E1B/E1B/BasicQuestions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1
+{
+    public class BasicQuestions
+    {
+        public static int OddSum(int[] nums)
+        {
+            int sum = 0;
+            foreach(int num in nums)
+            {
+                if (num % 2 != 0)
+                    sum += num;
+            }
+            return sum;
+        }
+
+        public static void Swap<_Type>(ref _Type a, ref _Type b)
+        {
+            _Type tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
---
+++ E1B/E1B/Human.cs
@@ -0,0 +1,40 @@
+﻿namespace E1B
+{
+    public interface IHasAge
+    {
+        int GetAge();
+    }
+    public class Human : IHasAge
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public Human(string name , int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+    }
+}
