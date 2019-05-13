8963b18f2c730415ea6e59301389b4f07d4eb17a
ghzamani <ghzamaninejad@gmail.com>
2019-05-13 14:56:30 +0430

    TA

---
+++ tamrin/tamrin/PriceFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace tamrin
+{
+    public interface IFilter
+    {
+        bool Filter(Item item);
+    }
+
+    public class PriceFilter : IFilter
+    {
+        public double Price;
+        
+        public bool Filter(Item item)
+        {
+            return Price >= item.Price;
+        }
+    }
+
+    public class PriceRangeFilter : IFilter
+    {
+        public double minPrice;
+        public double maxPrice;
+
+        public bool Filter(Item item)
+        {
+            if (item.Price <= minPrice && item.Price >= maxPrice)
+                return true;
+            return false;
+        }
+    }
+
+    public class ReleasedDateFilter : IFilter
+    {
+        public DateTime Date;
+        public bool Filter(Item item)
+        {
+            return item.RealeseDate >= Date;
+        }
+    }
+
+    public class BrandFilter : IFilter
+    {
+        public string MyBrand; 
+        public bool Filter(Item item)
+        {
+            return item.Brand == MyBrand;
+        }
+    }
+}
---
+++ tamrin/tamrin/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public DateTime RealeseDate { get; set; }
+        public string Brand { get; set; }
+
+        public Item(int id,string title, double price, 
+            DateTime realeseDate, string brand)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Price = price;
+            this.RealeseDate = realeseDate;
+        }
+
+        public Item() { }
+
+        public double UpdatePrice (double newPrice)
+        {
+            this.Price = 1.2 * newPrice;  
+            return (newPrice >= Price ? Price : newPrice);
+        }
+
+        /*public void AddToCart (Customer customer)
+        {
+
+        }*/
+    }
+
+    
+
+    public class ItemInventory 
+    {
+        public List<Item> Items = new List<Item>();
+
+        public List<Item> Filter (List<IFilter> filters)
+        {
+            List<Item> Result = new List<Item>();
+            foreach (var item in Items)
+            {
+                bool AllPassed = true;
+                foreach(var filter in filters)
+                {
+                    if (!filter.Filter(item))
+                        AllPassed = false;
+                }
+                if (AllPassed)
+                    Result.Add(item);
+            }
+            return Result;
+        }
+
+        public Item GetItemById(int id)
+        {
+            foreach(var item in this.Items)
+            {
+                if (item.Id == id)
+                    return item;
+            }
+            return new Item();
+        }
+
+        public int AddItem(Item item)
+        {
+            Items.Add(item);
+            return Items.Count;
+        }
+
+        public bool RemoveItem(Item item)
+        {
+            if (Items.Contains(item))
+            {
+                Items.Remove(item);
+                return true;
+            }
+            return false;
+        }
+    }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
