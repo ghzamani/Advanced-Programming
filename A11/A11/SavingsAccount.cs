df04411b89a7db3cbd080d11906fdc4879e23a89
ghzamani <ghzamaninejad@gmail.com>
2019-05-25 16:43:52 +0430

    A11

---
+++ A11/A11/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace A11
+{
+    public class SavingsAccount : Account
+    {
+        public double InterestRate;
+
+        public SavingsAccount(double balance, double interestRate) : base(balance)
+        {
+            this.InterestRate = interestRate;
+        }
+
+        public override void Credit(double amount)
+        {
+            base.Credit(amount);
+        }
+
+        public override bool Debit(double amount) => base.Debit(amount);
+
+        public double CalculateInterest()
+        {
+            return Balance * InterestRate;
+        }
+
+
+    }
+}
