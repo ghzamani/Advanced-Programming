5e1c5ba969b816fc9af9747306d7c4c8beb1f425
ghzamani <ghzamaninejad@gmail.com>
2019-06-28 22:41:40 +0430

    A14 is implemented

---
+++ A14/A14/PointState.cs
@@ -0,0 +1,32 @@
+﻿namespace A14
+{
+    /// <summary>
+    /// این وضعیت نشان دهنده حالتی است که نقطه زده شده
+    /// این وضعیت شبیه وضعیت
+    /// Accumulate
+    /// است
+    /// تنها فرقش این است که نقطه جدیدی نمی شود زد.
+    /// تغییرات لازم را برای این کار بدهید.
+    /// </summary>
+    public class PointState : AccumulateState
+    {
+        public PointState(Calculator calc) : base(calc) { }
+
+        //#1 لطفا!
+        
+        //اگر کاربر عدد وارد کرد باید وارد
+        //AccumulateState
+        //بشویم، چون این کلاس از کلاس 
+        //AccumulateState
+        //به ارث برده پس تمامی متدهای آن کلاس را دارد
+        
+        //اگر کاربر نقطه وارد کرد باید در همین 
+        //state
+        //بمانیم
+        public override IState EnterPoint()
+        {
+            return this;
+        }
+             
+    }
+}
---
+++ A14/A14/StartState.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace A14
+{
+    /// <summary>
+    /// این کلاس بطور کامل پیاده سازی شده است و نیاز به تغییر ندارد.
+    /// تنها تغییرات لازم کامنت های شماست 
+    /// </summary>
+    public class StartState : CalculatorState
+    {
+        public StartState(Calculator calc) : base(calc) { }
+
+        public override IState EnterEqual() => 
+            ProcessOperator(new ComputeState(this.Calc));
+        //اگر کاربر در ابتدا = را وارد کند چون 
+        //ProcessOperator
+        // به اکسپشن نمیخورد با پیغامی مواجه نمی شود و وارد        
+        //ComputeState
+        //می شود
+
+
+        public override IState EnterZeroDigit()
+        {
+            //تا زمانی که کاربر صفر وارد میکند باید در همین 
+            //state
+            //باقی بماند
+            this.Calc.Display = "0";
+            return this;
+        }
+
+        public override IState EnterNonZeroDigit(char c)
+        {
+            //به محض وارد کردن عددی غیر 0 
+            //state
+            //تغییر میکند
+            this.Calc.Display = c.ToString();
+            return new AccumulateState(this.Calc);
+        }
+
+        public override IState EnterOperator(char c) => 
+            ProcessOperator(new ComputeState(this.Calc), c);
+
+        public override IState EnterPoint()
+        {
+            this.Calc.Display = "0.";
+            return new PointState(this.Calc);
+        }
+    }
+}
